8d217ea2ba6884280239f597b96b15afa4f5d9ca
Ahmet-Akpolat <stepiem@outlook.com>
2024-01-19 03:41:25 +0300

    AnnouncementService Updated

--- src/services/AnnouncementService.ts
+++ src/services/AnnouncementService.ts
@@ -1,12 +1,10 @@
 import axiosInstance from '../utilities/axiosInterceptors';
  
 class AnnouncementService {
-  async getAnnouncement(id: string) {
+  async getAnnouncement() {
     try {
       const response = await axiosInstance.get(`StudentAnnouncements?PageIndex=0&PageSize=999`);
-      const studentAnnouncement = response.data.items.filter((item:any) => item.studentId === id);
-      console.log(studentAnnouncement);
-      return studentAnnouncement;
+      return response.data.items;
     } catch (error) {
       console.error('API çağrısı sırasında bir hata oluştu:', error);
       throw error; // Hata bilgisini dışarı fırlat
