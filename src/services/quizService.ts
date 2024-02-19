4add69e87daf6334455564ccb46258b2a869be18
yugurlu <yusufugurlu39@outlook.com>
2024-02-19 19:48:03 +0300

    FINISHHH?

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -1,31 +1,35 @@
 import { BaseService } from "./baseService";
 import axiosInstance from "../utils/axiosInterceptors";
-import { GetByIdQuizResponse, GetListQuizListItemDto } from "../models/responses/QuizResponses";
+import {
+  GetByIdQuizResponse,
+  GetListQuizListItemDto,
+} from "../models/responses/QuizResponses";
 import { AxiosResponse } from "axios";
 import { CreateStudentQuizOptionRequest } from "../models/requests/StudentQuizOptionRequest";
 import { CreateStudentQuizResultRequest } from "../models/requests/StudentQuizResultRequests";
 
-class QuizService  {
-
+class QuizService {
   async GetForAllStudent() {
-    const response =await axiosInstance.get("GeneralQuizs"+"?PageIndex="+0+"&PageSize="+5);
+    const response = await axiosInstance.get(
+      "GeneralQuizs" + "?PageIndex=" + 0 + "&PageSize=" + 5
+    );
     return response.data;
-  }  
+  }
   async GetForClassStudent() {
-    const response =await axiosInstance.get("ClassQuizs"+"?PageIndex="+0+"&PageSize="+100);
+    const response = await axiosInstance.get(
+      "ClassQuizs" + "?PageIndex=" + 0 + "&PageSize=" + 100
+    );
     return response.data;
-  }  
+  }
   async getById(id: number) {
     const response = await axiosInstance.get<GetByIdQuizResponse>(
       `Quizs/${id}`
     );
-    return response; 
+    return response;
   }
   async getQuizSessionById(id: number) {
-    const response = await axiosInstance.get<any>(
-      `Quizs/quizSession/${id}`
-    );
-    return response; 
+    const response = await axiosInstance.get<any>(`Quizs/quizSession/${id}`);
+    return response;
   }
   async addQuizOption(request: CreateStudentQuizOptionRequest) {
     return axiosInstance.post("StudentQuizOptions", request);
@@ -34,13 +38,9 @@ class QuizService  {
     return axiosInstance.post("StudentQuizResults", request);
   }
   async getByQuizId(quizId: number) {
-    const response = await axiosInstance.get(
-      `StudentQuizResults/${quizId}`
-    );
+    const response = await axiosInstance.get(`StudentQuizResults/${quizId}`);
     return response.data; // Directly returning the data part of the response
   }
-
-  
 }
 
 export default new QuizService();
