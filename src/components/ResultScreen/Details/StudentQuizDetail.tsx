5596f4dd5e02b629cba63092d7ae1bc110a7c628
yugurlu <yusufugurlu39@outlook.com>
2024-02-23 18:21:44 +0300

    Refactor CSS and add button styling

--- src/components/ResultScreen/Details/StudentQuizDetail.tsx
+++ src/components/ResultScreen/Details/StudentQuizDetail.tsx
@@ -1,105 +1,90 @@
 import { useEffect, useState } from "react";
-import { Modal } from "react-bootstrap";
-import { ClipLoader } from "react-spinners";
-import { toast } from "react-toastify";
-import './StudentQuizDetail.css'
 import quizervice from "../../../services/quizService";
+import "./StudentQuizDetail.css";
 
 type Props = {
-    quizId: number | undefined;
+  quizId: number | undefined;
 };
 
 const ExamModal = (props: Props) => {
-    const [quiz, setquiz] = useState<any>();
-    const [choosenOption, setChoosenOption] = useState();
-    const [quizCount,setquizCount]=useState(0);
-    const [examModalControl, setExamModalControl] = useState<boolean>();
-    const [minuteControl, setMinuteControl] = useState(true);
-    const [show, setModalShow] = useState(false);
+  const [quiz, setquiz] = useState<any>();
 
-    const getquiz = async () => {
-        if (props.quizId != undefined) {
-            await quizervice.getByQuizDetailWithByQuizId(props.quizId).then((r) => {
-                setquiz(r);
-                console.log(r);
-                
-                console.log(r.studentOptions);
-                
-            });
-        }
-    };
-    useEffect(() => {
-        getquiz();
+  const getquiz = async () => {
+    if (props.quizId != undefined) {
+      await quizervice.getByQuizDetailWithByQuizId(props.quizId).then((r) => {
+        setquiz(r);
+        console.log(r);
 
-    }, []);
+        console.log(r.studentOptions);
+      });
+    }
+  };
+  useEffect(() => {
+    getquiz();
+  }, []);
 
-    return (
+  return (
+    <>
+      {
         <>
-
-            {(
-                <>
-                    <div className="question">
-                        <div className="text-center" style={{fontSize:'xx-large'}}>
-                            {quiz?.name}
-                        </div>
+          <div className="question">
+            <div className="text-center" style={{ fontSize: "xx-large" }}>
+              {quiz?.name}
+            </div>
+          </div>
+          <div className="question">
+            <div className="progress-bar"></div>
+            {quiz?.questions.map((question: any) => (
+              <div className="main ">
+                <div className="title text-center">
+                  <p>
+                    <span>{question.sentence}</span>
+                  </p>
+                  <p>
+                    {question.imageUrl != null && (
+                      <img
+                        className="image_resized"
+                        style={{ width: "78.26%" }}
+                        src={question.imageUrl}
+                        alt="44.png"
+                        sizes="100vw"
+                      />
+                    )}
+                  </p>
+                </div>
+                <div className="options d-flex justify-content-center">
+                  {question.questionOptions.map((option: any) => (
+                    <div
+                      className={`option text-center w-75 ${
+                        option.option.id === question.correctOptionId
+                          ? "active"
+                          : ""
+                      } ${
+                        quiz.studentOptions.some(
+                          (d: any) =>
+                            d.questionId == question.id &&
+                            d.optionId == option.option.id
+                        ) && "wrong"
+                      }`}
+                    >
+                      <p>
+                        {option.option.text}
+                        {quiz.studentOptions.some(
+                          (d: any) =>
+                            d.questionId == question.id &&
+                            d.optionId == option.option.id
+                        ) && " (Bu Seçeneği Seçtiniz)"}
+                      </p>
                     </div>
-                    <div className="question">
-                        <div className="progress-bar"></div>
-                        {quiz?.questions.map((question:any)=>(
-                             <div className="main ">
-                             <div className="title text-center">
-                                 <p>
-                                     <span>
-                                         {
-                                             question.sentence
-                                         }
-                                         
-                                     </span>
-                                 </p>
-                                 <p>
-                                     {question.imageUrl!=null && (
-                                             <img
-                                                 className="image_resized"
-                                                 style={{ width: "78.26%" }}
-                                                 src={
-                                                    question.imageUrl
-                                                 }
-                                                 alt="44.png"
-                                                 sizes="100vw"
-                                             />
-                                         )}
-                                 </p>
-                             </div>
-                             <div className="options">
-                                 {question.questionOptions.map((option: any) => (
-                                    
-                                     <div
-                                     className={`option text-center ${
-                                        option.option.id === question.correctOptionId
-                                          ? "active"
-                                          : ""
-                                      } ${quiz.studentOptions.some((d:any)=>d.questionId==question.id&&d.optionId==option.option.id)&&"wrong"}`}
-                                         
-                                     >
-                                         <p>{option.option.text}{quiz.studentOptions.some((d:any)=>d.questionId==question.id&&d.optionId==option.option.id)&&" (Bu Seçeneği Seçtiniz)"}</p>
-                                         
-                                     </div>
-                                 ))}
-                             </div>
-                         </div>
-                        ))}
-                       
-                        {/* <button onClick={() => setShowDetails(true)} className="btn btn-primary mt-8 ms-auto me-auto" style={{ width: "max-content" }}>
-                            Detayları Görüntüle
-                        </button> */}
-                    </div>
-                </>
-            )}
-
-
-
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </>
-    );
+      }
+    </>
+  );
 };
 
 export default ExamModal;
--- src/components/ResultScreen/ResultScreen.tsx
+++ src/components/ResultScreen/ResultScreen.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const ResultScreen = (props: Props) => {
   const [results, setResults] = useState<any>();
-  const [showDetail,setShowDetails]=useState<boolean>(false);
+  const [showDetail, setShowDetails] = useState<boolean>(false);
   const getResult = async () => {
     if (props.quizId != undefined) {
       await quizService.getByQuizId(props.quizId).then((response) => {
@@ -38,32 +38,44 @@ const ResultScreen = (props: Props) => {
           <div className="modal-content">
             <Modal.Body>
               <div className="quiz-screen">
-                {!showDetail&&true?(
-                <div className="result-screen ">
-                  <span className="result-title">Test Sonucu</span>
-                  <div className="result-items col-15">
-                    <span className="d-flex flex-column col-md-2 h-50">
-                      {results?.correctAnswerCount}
-                      <a> Doğru</a>
-                    </span>
-                    <span className="d-flex flex-column col-md-2">
-                      {results?.wrongAnswerCount}
-                      <a>Yanlış</a>
-                    </span>
-                    <span className="d-flex flex-column col-md-2">
-                      {results?.emptyAnswerCount}
-                      <a>Boş</a>
-                    </span>
-                    <span className="d-flex flex-column col-md-2">
-                      {results?.point} <a>Puan</a>
-                    </span>
-                  </div>
-                  <div className="row">
-                    <button onClick={()=>setShowDetails(true)} className="btn btn-primary mt-8 ms-auto me-auto" style={{width:"max-content"}}>
-                      Detayları Görüntüle
+                {!showDetail && true ? (
+                  <div className="result-screen ">
+                    <span className="result-title">Test Sonucu</span>
+                    <div className="result-items col-15">
+                      <span className="d-flex flex-column col-md-2 h-50">
+                        {results?.correctAnswerCount}
+                        <a> Doğru</a>
+                      </span>
+                      <span className="d-flex flex-column col-md-2">
+                        {results?.wrongAnswerCount}
+                        <a>Yanlış</a>
+                      </span>
+                      <span className="d-flex flex-column col-md-2">
+                        {results?.emptyAnswerCount}
+                        <a>Boş</a>
+                      </span>
+                      <span className="d-flex flex-column col-md-2">
+                        {results?.point} <a>Puan</a>
+                      </span>
+                    </div>
+                    <div className="row">
+                      <button
+                        onClick={() => setShowDetails(true)}
+                        className="quiz-btn btn btn-primary mt-8 ms-auto me-auto"
+                        style={{
+                          width: "max-content",
+                          borderRadius: "50px",
+                          background: "#9933ff",
+                          color: "white",
+                          transition: "0.5s",
+                          border: "none",
+                        }}
+                      >
+                        Detayları Görüntüle
                       </button>
                     </div>
-                </div>):(
+                  </div>
+                ) : (
                   <StudentQuizDetail quizId={props.quizId} />
                 )}
               </div>
