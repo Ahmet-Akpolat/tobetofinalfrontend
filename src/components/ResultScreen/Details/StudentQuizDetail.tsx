8c7d1c73caa054d56247931522eb3c17d8fd6667
Koyuncu <130415130+koyuncu5362@users.noreply.github.com>
2024-02-24 13:24:55 +0300

    fixed quiz detail and examsession

--- src/components/ResultScreen/Details/StudentQuizDetail.tsx
+++ src/components/ResultScreen/Details/StudentQuizDetail.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import quizervice from "../../../services/quizService";
 import "./StudentQuizDetail.css";
 import { ClipLoader } from "react-spinners";
+import { number } from "yup";
 
 type Props = {
   quizId: number | undefined;
@@ -10,12 +11,14 @@ type Props = {
 const ExamModal = (props: Props) => {
   const [quiz, setquiz] = useState<any>();
   const [loading, setLoading] = useState(false);
+  const [questionCount,setQuestionCount]=useState(number);
 
   const getquiz = async () => {
     setLoading(true);
     if (props.quizId != undefined) {
       await quizervice.getByQuizDetailWithByQuizId(props.quizId).then((r) => {
         setquiz(r);
+        
       });
     }
     setLoading(false);
@@ -35,36 +38,52 @@ const ExamModal = (props: Props) => {
             size={50}
           />
         ) : (
-          <div>
-            <div className="question">
+          <div className="xx">
+            <div className="question2">
               <div className="text-center" style={{ fontSize: "xx-large" }}>
                 {quiz?.name}
               </div>
             </div>
-            <div className="question">
+            <div className="question2">
               <div className="progress-bar"></div>
               {quiz?.questions.map((question: any) => (
                 <div className="main ">
+                   
                   <div className="title text-center">
+                  {quiz.studentOptions.some(
+                            (d: any) =>
+                              d.questionId == question.id &&
+                              d.optionId == question.correctOptionId
+                          ) && (<div className="btnx successsbtn text-center"><a >Doğru</a></div>)}
+                    {quiz.studentOptions.some(
+                            (d: any) =>
+                              d.questionId == question.id &&
+                              d.optionId != question.correctOptionId
+                          ) && (<div className="text-center btnx wrongButton"><a >Yanlış</a></div>)}
+                    {!quiz.studentOptions.some(
+                            (d: any) =>
+                              question.id==d.questionId
+                          ) && (<div className="text-center btnx emptyButton"><a >İşaretlenmedi</a></div>)}
                     <p>
+                   
                       <span>{question.sentence}</span>
                     </p>
-                    <p>
-                      {question.imageUrl != null && (
+                  
+                      {question.imageUrl != null && (  <p>
                         <img
                           className="image_resized"
                           style={{ width: "78.26%" }}
                           src={question.imageUrl}
                           alt="44.png"
                           sizes="100vw"
-                        />
+                        /></p>
                       )}
-                    </p>
+                    
                   </div>
                   <div className="options d-flex justify-content-center">
                     {question.questionOptions.map((option: any) => (
                       <div
-                        className={`option text-center w-75 ${
+                        className={`option2 text-center w-75 ${
                           option.option.id === question.correctOptionId
                             ? "active"
                             : ""
@@ -82,9 +101,10 @@ const ExamModal = (props: Props) => {
                             (d: any) =>
                               d.questionId == question.id &&
                               d.optionId == option.option.id
-                          ) && " (Bu Seçeneği Seçtiniz)"}
+                          ) && " (Seçiminiz)"}
                         </p>
                       </div>
+                      
                     ))}
                   </div>
                 </div>
